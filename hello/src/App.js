4523b32b04210284a244f947ee41cecee6e5ff61
fahime <abouhamze.fahime@gmail.com>
2022-11-30 15:34:30 -0800

    session

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,23 +1,34 @@
 
 import './App.css';
+import Employee from './Components/Employee';
+import {useState} from 'react';
 
 function App() {
+  const [role , setRole]=useState('dev');
+
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-
-        <p>
-         HeLLLLLLLLLLLLLLLLLLLLo 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('inside the return')}
+      {showEmployees ?
+        (
+          <>
+          <input type="text" 
+          onChange={(e)=>{
+               console.log(e.target.value);   
+               setRole(e.target.value);    
+          }}
+          />
+            <Employee name=" fahime" role="admin"/>
+            <Employee name=" Arash" role={role}/>
+            <Employee name=" Nima" />
+          </>
+        )
+     :
+     (
+      <p>You cannot see the employees</p>
+     )
+    }
     </div>
   );
 }
